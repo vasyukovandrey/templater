7ecb9499f822533acdf233c7fa66c1338c7dc8c6
vasyukovandrey <vasyukovandrey@yandex.ru>
2020-01-27 02:01:48 +0300

    Черновик класса без возврата данных

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 "use strict";
+
+class LoadResource {
+    constructor(fileUrl) {
+        this.fileUrl = fileUrl;
+        this.result = false;
+    }
+
+    xhrSuccess(xhr) {
+        this.result = xhr.response;
+        return this.result;
+    }
+    
+    xhrError() {
+        console.error(`error: ${this.status}; request status: ${this.statusText}`); 
+    }
+
+    responseData() {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.onload = this.xhrSuccess.bind(this,xhr);
+        xhr.onerror = this.xhrError.bind(xhr);
+        xhr.open("GET", this.fileUrl, true);
+        xhr.send();
+    }
+
+    getData(){
+        return this.result;
+    }
+}
+
 const fileUrl = "https://raw.githubusercontent.com/mdn/learning-area/master/javascript/oojs/json/superheroes.json";
-let data = new Object();
-
-function xhrSuccess() {
-    this.callback.apply(this);
-}
-
-function xhrError() { 
-    console.error('error: ${this.status}; request status: ${this.statusText}'); 
-}
-
-function loadFile(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.callback = callback;
-    xhr.responseType = 'json';
-    xhr.onload = xhrSuccess;
-    xhr.onerror = xhrError;
-    xhr.open("GET", url, true);
-    xhr.send();
-}
-
-function cunstruct() {
-    data = this.response;
-}
-
-loadFile(fileUrl, cunstruct);
+const xhr = new LoadResource(fileUrl);
+xhr.responseData();
+const data = xhr.getData();
+console.log(data);
