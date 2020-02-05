09fc9f689c8db15c3f191855cabd793ad1a148f0
vasyukovandrey <vasyukovandrey@yandex.ru>
2020-02-05 20:34:17 +0300

    Начал строить DOM из объекта

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ const fileUrl = "https://raw.githubusercontent.com/mdn/learning-area/master/java
 const xhr = new LoadResource(fileUrl);
 const data = xhr.responseData();
 console.log(data);
+
+const squadName = `<h1> ${data.squadName} </h1>`;
+document.getElementById('root').innerHTML += squadName;
+
+const homeTown = `<div> ${data.homeTown} </div>`;
+document.getElementById('root').innerHTML += homeTown;
+
+const formed = `<div> ${data.formed} </div>`;
+document.getElementById('root').innerHTML += formed;
+
+const secretBase = `<i> ${data.secretBase} </i>`;
+document.getElementById('root').innerHTML += secretBase;
