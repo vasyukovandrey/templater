245324515639b167f6fbf599cb4499e9475c6d0b
vasyukovandrey <vasyukovandrey@yandex.ru>
2020-02-07 21:00:58 +0300

    Переписал составление фрагмента на отдельную функцию

--- index.js
+++ index.js
@@ -16,28 +16,35 @@ class LoadResource {
     }
 }
 
+function buildDom (items,rules) {
+    const fragment = document.createDocumentFragment();
+    let tag = '';
+
+    for (let item in items) {
+        if (item !== 'members' && item !== 'active') {
+            if  (rules[item]) {
+                tag = rules[item];
+            } else {
+                tag = 'div';
+            }
+            const node = document.createElement(tag);
+            node.innerText = items[item];
+            fragment.appendChild(node);
+        }
+    }
+
+    return fragment;
+}
+
 const fileUrl = "https://raw.githubusercontent.com/mdn/learning-area/master/javascript/oojs/json/superheroes.json";
 const xhr = new LoadResource(fileUrl);
 const data = xhr.responseData();
-console.log(data);
-
 const root = document.getElementById('root');
-const fragment = document.createDocumentFragment();
 
-const squadName  = document.createElement("h1");
-squadName.innerText = data.squadName;
-fragment.appendChild(squadName);
+const tagRules = {
+    "squadName": "h1",
+    "secretBase": "i"
+};
 
-const homeTown  = document.createElement("div");
-homeTown.innerText = data.homeTown;
-fragment.appendChild(homeTown);
-
-const formed  = document.createElement("div");
-formed.innerText = data.formed;
-fragment.appendChild(formed);
-
-const secretBase  = document.createElement("i");
-secretBase.innerText = data.secretBase;
-fragment.appendChild(secretBase);
-
-root.append(fragment);
+const segment = buildDom(data,tagRules);
+root.append(segment);
